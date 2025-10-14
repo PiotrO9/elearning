af1329f5d79ffca5f97be02841e00653f09f10ea
Daniel <daniel.bernat000@gmail.com>
2025-10-14 22:52:46 +0200

    feat: add Navbar and Home placeholders

---
+++ FE/src/components/Navbar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterLink } from 'vue-router';
+import MaxWidthWrapper from './wrappers/MaxWidthWrapper.vue';
+
+</script>
+
+<template>
+    <MaxWidthWrapper class="flex justify-between items-center py-4">
+        <RouterLink to="/" class="italic font-semibold text-2xl">eLearning</RouterLink>
+        <nav class="flex space-x-4 text-sm">
+            <RouterLink to="/courses">Kursy</RouterLink>
+            <RouterLink to="/login">Logowanie</RouterLink>
+        </nav>
+    </MaxWidthWrapper>
+</template>
