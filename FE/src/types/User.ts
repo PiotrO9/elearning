ae4f69d0dc99b67e2c598ca7ca8437f2a6c75f54
Daniel <daniel.bernat000@gmail.com>
2025-10-15 16:13:23 +0200

    feat: add user types

---
+++ FE/src/types/User.ts
@@ -0,0 +1,23 @@
+import * as z from "zod";
+
+export interface User {
+    email: string;
+    username: string;
+    createdAt: string;
+    lastSeen: string;
+}
+
+export const RegisterScheme = z.object({
+    email: z.email({ message: "Nieprawidłowy adres email" }),
+    username: z.string()
+        .min(3, { message: "Nazwa użytkownika musi zawierać co najmniej 3 znaki" })
+        .max(30, { message: "Nazwa użytkownika musi zawierać maksymalnie 30 znaków" }),
+    password: z.string().min(8, { message: "Hasło musi zawierać co najmniej 8 znaków" })
+})
+
+export const LoginScheme = z.object({
+    email: z.email({ message: "Nieprawidłowy adres email" }),
+    password: z.string().min(6, { message: "Hasło musi zawierać co najmniej 6 znaków" }).max(100, { message: "Hasło może zawierać maksymalnie 100 znaków" }),
+})
+
+export type AuthResponse = { success: boolean, message: string, data: User}
