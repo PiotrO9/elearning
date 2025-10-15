36284aa1c1cacf9e62cb827102d926008299b7ee
Daniel <daniel.bernat000@gmail.com>
2025-10-15 18:35:20 +0200

    feat: add Input component

---
+++ FE/src/components/ui/Input.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+    defineProps<{
+
+    }>()
+</script>
+
+
+<template>
+    <input type="text" class="bg-zinc-100 focus:bg-zinc-200 outline-0 duration-300 p-2 rounded">
+</template>
